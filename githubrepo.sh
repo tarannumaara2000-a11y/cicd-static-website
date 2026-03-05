42abbd86c4f3af3dc5f51bc10af290cd406b383a
root <root@ip-10-0-1-119.ap-south-1.compute.internal>
2026-03-05 19:52:56 +0000

    Initial commit

---
+++ githubrepo.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/tarannumaara2000-a11y/cicd-static-website.git
+git push -u origin main
